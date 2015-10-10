18180ddd3bcc479d109cda19490fe15f2e256934
berlinisimus <berlinisimus.92@gmail.com>
2015-10-10 13:48:16 +0300

    add file

---
+++ lesson1/src/lesson1/Converter.java
@@ -0,0 +1,27 @@
+package lesson1;
+
+/**
+ * Created by Alex on 10/10/2015.
+ */
+import java.util.Scanner;
+
+/**
+ * Created by Alex on 10/8/2015.
+ */
+
+public class Converter {
+    public void convertDigitToWord() {
+        int digit;
+        String arrayOfWords[] = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter digit to convert");
+        digit = input.nextInt();
+
+        if(digit < 0 || digit > 9){
+            System.out.print("Please, enter digit >= 0 and <= 9");
+        }else{
+            System.out.print(digit + " -> " + arrayOfWords[digit]);
+        }
+    }
+}
